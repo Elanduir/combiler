0f0a635c1c78eaa89e349935889ab48d82cc40a2
Pascal <pascal.huerzeler@students.fhnw.ch>
2021-11-06 09:21:20 +0100

    - begin of Parser

--- compiler/src/main/java/Parser/Parser.java
+++ compiler/src/main/java/Parser/Parser.java
@@ -1,4 +1,31 @@
 package Parser;
 
+import Scanner.Base;
+import Scanner.LexicalAnalysisScanner.*;
+import Scanner.Terminals;
+
+import java.util.List;
+
 public class Parser {
+
+    private List<Base> tokens;
+    private Base currentToken;
+    private int counter;
+    private Terminals currentTerminal;
+
+    public Parser(List<Base> tokens) {
+        this.tokens = tokens;
+        nextToken();
+    }
+
+    private void nextToken() {
+        currentToken = tokens.get(counter++);
+        currentTerminal = currentToken.getTerminal();
+    }
+
+    //consume
+
+
+    //parse
+
 }
